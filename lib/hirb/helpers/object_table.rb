9453bee7169fe2d20ff7e2359b0c736f2f81af95
Will McDonald <wemcdonald@gmail.com>
2017-01-19 09:55:29 -0800

    Allow fetching of nested object values. E.g.: puts Hirb::Helpers::AutoTable.render(person, fields: %w{name company.president.name})

--- lib/hirb/helpers/object_table.rb
+++ lib/hirb/helpers/object_table.rb
@@ -7,8 +7,19 @@ class Hirb::Helpers::ObjectTable < Hirb::Helpers::Table
     options[:fields] ||= [:to_s]
     options[:headers] ||= {:to_s=>'value'} if options[:fields] == [:to_s]
     item_hashes = options[:fields].empty? ? [] : Array(rows).inject([]) {|t,item|
-      t << options[:fields].inject({}) {|h,f| h[f] = item && item.__send__(f); h}
+      t << options[:fields].inject({}) {|h,f| h[f] = item && get_item_val(item, f); h }
     }
     super(item_hashes, options)
+  end
+
+  def self.get_item_val(item, field)
+    return item if item.nil?
+
+    field, remaining = field.to_s.split('.', 2)
+    if remaining
+      get_item_val(item.send(field), remaining)
+    else
+      item.send(field)
+    end
   end
 end
